eacdd0d24760af6ddf2f39146fedeedf7be78bfb
Deepak Raj <deepacraj616@gmail.com>
2018-08-25 15:53:02 +0530

    Initial commit

---
+++ src/main/java/com/netent/asg/RandomGameApplication.java
@@ -0,0 +1,12 @@
+package com.netent.asg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGameApplication.class, args);
+	}
+}
